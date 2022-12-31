210e2ff7a913640e0a861b996fb8823242f84d25
Tomonori Tachi <67524847+tomosukesan@users.noreply.github.com>
2022-12-31 22:47:53 +0900

    adjust

--- bonus/ft_hex_print_bonus.c
+++ bonus/ft_hex_print_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ttachi <ttachi@student.42tokyo.ja>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 18:52:38 by ttachi            #+#    #+#             */
-/*   Updated: 2022/12/28 18:50:24 by ttachi           ###   ########.fr       */
+/*   Updated: 2022/12/31 22:47:17 by ttachi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,24 +44,7 @@ int	ft_hex_process(va_list ap, char *rule, t_flags flag)
 	x = va_arg(ap, unsigned int);
 	if (x == 0)
 		flag.sharp = FALSE;
-	digit = ft_cal_hex_digit(x);
-/*
-	if (flag.width > flag.precision && flag.width > digit)
-	{
-		if (flag.width < digit + flag.sharp)
-		{
-
-		}
-	}
-	else if (flag.precision > flag.width && flag.precision > digit)
-	{
-
-	}
-	else
-	{
-
-	}
-*/
+	digit = ft_cal_hex_digit((unsigned long)x);
 	result = flag.width;
 	if (flag.precision > digit)
 	{
